5410b6455e2f16d920199fb7b4d916f112036269
SuyashShekhar <106506584+SuyashShekhar@users.noreply.github.com>
2023-10-08 04:38:04 +0000

    container config done

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,14 @@
+"use strict"; // treat all js code as newer version
+
+let name = "Suyash" // string
+let age = 23;  // number
+let loggedIn = false;  // boolean
+
+// other datatypes in js
+// null => standalone value
+// undefined => value not defined
+// symbol => for uniqueness
+// object
+
+console.log(typeof null); // output - object
+console.log(typeof undefined); // output - undefined
